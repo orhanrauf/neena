5bf3f35ccd7e2c8b7ea81f9d6f0fd632dd3e19b1
Jacob <jacobtimmer04@gmail.com>
2023-07-27 10:51:21 +0200

    Tasks page WIP

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,32 +1,38 @@
 /* eslint-disable import/order */
-import '@/@iconify/icons-bundle'
-import App from '@/App.vue'
-import layoutsPlugin from '@/plugins/layouts'
-import vuetify from '@/plugins/vuetify'
-import { loadFonts } from '@/plugins/webfontloader'
-import router from '@/router'
-import '@core/scss/template/index.scss'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import '@styles/styles.scss'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
+import "@/@iconify/icons-bundle";
+import App from "@/App.vue";
+import layoutsPlugin from "@/plugins/layouts";
+import vuetify from "@/plugins/vuetify";
+import { loadFonts } from "@/plugins/webfontloader";
+import router from "@/router";
+import "@core/scss/template/index.scss";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import "@styles/styles.scss";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import dayjs from "dayjs";
+import "dayjs/locale/en";
 
-loadFonts()
+dayjs.locale("en");
+
+loadFonts();
 
 // Create vue app
-const app = createApp(App)
+const app = createApp(App);
 
 // Use plugins
-app.use(vuetify)
-app.use(createPinia())
-app.use(router)
-app.use(layoutsPlugin)
-app.use(ElementPlus)
+app.use(vuetify);
+app.use(createPinia());
+app.use(router);
+app.use(layoutsPlugin);
+app.use(ElementPlus);
+// @ts-ignore
+app.use(dayjs);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+  app.component(key, component);
 }
 
 // Mount vue app
-app.mount('#app')
+app.mount("#app");
--- frontend/src/pages/tasks/index.vue
+++ frontend/src/pages/tasks/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { VDataTable } from "vuetify/labs/VDataTable";
+import axios from "@axios";
+import dayjs from "dayjs";
 
 const options = ref({
   page: 1,
@@ -7,153 +9,52 @@ const options = ref({
   sortBy: [""],
   sortDesc: [false],
 });
+
+const loading = ref(false);
 const search = ref("");
 
 // Items per page options
 const itemsPerPage = [5, 10, 25, 50, 100];
 
-// Geaders
+// Headers
 const headers = [
-  { title: "NAME", key: "name" },
-  { title: "PARAMETERS", key: "parameters" },
-  { title: "CREATED DATE", key: "created_date" },
+  { title: "NAME", key: "task_name" },
+  { title: "PARAMETERS", key: "parameters_formatted" },
+  { title: "CREATED DATE", key: "created_date_formatted" },
   { title: "TAGS", key: "tags" },
   { title: "", key: "actions", sortable: false },
 ];
 
 // Data
-const items = [
-  {
-    name: "view_detail",
-    parameters: '<span class="text-green">string:</span> detail',
-    created_date: "09 May 2022",
-    tags: "system",
-  },
-  {
-    name: "lookup_acc_number_by_email",
-    parameters: '<span class="text-green">string:</span> email',
-    created_date: "19 Nov 2022",
-    tags: "custom, python, db",
-  },
-  {
-    name: "send_confirmation_email",
-    parameters:
-      '<span class="text-purple">bool:</span> confirmed, <span class="text-green">string:</span> messa...',
-    created_date: "25 Sep 2022",
-    tags: "custom, python, mail",
-  },
-  {
-    name: "view_detail",
-    parameters: '<span class="text-green">string:</span> detail',
-    created_date: "09 May 2022",
-    tags: "system",
-  },
-  {
-    name: "verify_age",
-    parameters: '<span class="text-blue">int:</span> age',
-    created_date: "25 Sep 2022",
-    tags: "custom, python, subs...",
-  },
-  {
-    name: "view_detail",
-    parameters: '<span class="text-green">string:</span> detail',
-    created_date: "09 May 2022",
-    tags: "system",
-  },
-  {
-    name: "view_detail",
-    parameters: '<span class="text-green">string:</span> detail',
-    created_date: "09 May 2022",
-    tags: "system",
-  },
-  {
-    name: "verify_age",
-    parameters: '<span class="text-blue">int:</span> age',
-    created_date: "25 Sep 2022",
-    tags: "custom, python, subs...",
-  },
-  {
-    name: "view_detail",
-    parameters: '<span class="text-green">string:</span> detail',
-    created_date: "09 May 2022",
-    tags: "system",
-  },
-  {
-    name: "view_detail",
-    parameters: '<span class="text-green">string:</span> detail',
-    created_date: "09 May 2022",
-    tags: "system",
-  },
-  {
-    name: "verify_age",
-    parameters: '<span class="text-blue">int:</span> age',
-    created_date: "25 Sep 2022",
-    tags: "custom, python, subs...",
-  },
-  // Additional items
-  {
-    name: "lookup_account_by_id",
-    parameters: '<span class="text-green">string:</span> id',
-    created_date: "12 Dec 2022",
-    tags: "custom, db",
-  },
-  {
-    name: "send_notification_email",
-    parameters: '<span class="text-green">string:</span> email',
-    created_date: "29 Nov 2022",
-    tags: "custom, mail",
-  },
-  {
-    name: "update_profile",
-    parameters:
-      '<span class="text-purple">bool:</span> isPublic, <span class="text-green">string:</span> name',
-    created_date: "05 Jan 2023",
-    tags: "custom, profile",
-  },
-  {
-    name: "calculate_total",
-    parameters: '<span class="text-blue">int:</span> quantity',
-    created_date: "18 Feb 2023",
-    tags: "custom, math",
-  },
-  {
-    name: "send_reset_password_email",
-    parameters: '<span class="text-green">string:</span> email',
-    created_date: "02 Mar 2023",
-    tags: "custom, mail",
-  },
-  {
-    name: "lookup_product_by_sku",
-    parameters: '<span class="text-green">string:</span> sku',
-    created_date: "15 Apr 2023",
-    tags: "custom, inventory",
-  },
-  {
-    name: "process_payment",
-    parameters:
-      '<span class="text-purple">bool:</span> isPaid, <span class="text-green">string:</span> paymentMethod',
-    created_date: "28 May 2023",
-    tags: "custom, payment",
-  },
-  {
-    name: "send_notification_sms",
-    parameters: '<span class="text-green">string:</span> phoneNumber',
-    created_date: "10 Jun 2023",
-    tags: "custom, sms",
-  },
-  {
-    name: "calculate_discount",
-    parameters: '<span class="text-blue">int:</span> amount',
-    created_date: "23 Jun 2023",
-    tags: "custom, math",
-  },
-  {
-    name: "lookup_order_by_id",
-    parameters: '<span class="text-green">string:</span> orderId',
-    created_date: "06 Jul 2023",
-    tags: "custom, db",
-  },
-];
+let items = ref([]);
+
+const taskDefinitions = computed(() => {
+  return items.value.map((item) => ({
+    ...item,
+    parameters_formatted: item.parameters
+      .map((param) => `${param.data_type}: ${param.name}`)
+      .join(", "),
+    created_date_formatted: dayjs(item.created_date).format("D MMM YYYY"),
+  }));
+});
+
+const getTasks = () => {
+  loading.value = true;
+
+  axios
+    .get("/v1/task_definitions/all", {
+      params: {
+        skip: 0,
+        limit: 10,
+      },
+    })
+    .then((response) => {
+      items.value = response.data;
+    })
+    .finally(() => (loading.value = false));
+};
+
+onMounted(() => getTasks());
 </script>
 
 <template>
@@ -186,7 +87,7 @@ const items = [
     <!-- Data Table  -->
     <VDataTable
       :headers="headers"
-      :items="items"
+      :items="taskDefinitions"
       :search="search"
       :items-per-page="options.itemsPerPage"
       :page="options.page"
