a35ca9a3a941245bb94424d5ac6820075af718fb
Rauf <rauf@neena.io>
2023-12-04 16:50:12 +0100

    Merge TF work

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 locals {
   resource_prefix = "${var.organization}-${var.project_name}-${var.environment}"
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,15 +1,7 @@
-output "function_app_default_hostname" {
-  value = module.function_app.default_hostname
-}
-
-output "function_app_principal_id" {
-  value = module.function_app.principal_id
-}
-
 output "resource_group_name" {
-  value = azurerm_resource_group.example_rg.name
+  value = azurerm_resource_group.rg.name
 }
 
 output "resource_group_location" {
-  value = azurerm_resource_group.example_rg.location
+  value = azurerm_resource_group.rg.location
 }
