d18ef63225c6e0e742df0922c4070ffe64dd7c13
Rauf <rauf@neena.io>
2024-02-09 21:32:06 +0100

    fix api formatting

--- frontend/src/composables/http.ts
+++ frontend/src/composables/http.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import store from '@/store';
 
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + 'api/v1/',
+  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1/',
 });
 
 http.interceptors.request.use(config => {
