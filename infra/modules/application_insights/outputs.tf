dae9cb1fe76bfcb02ad36a5fcd6505c82c04fd6a
Rauf <rauf@neena.io>
2024-02-11 18:26:49 +0100

    Fix naming terraform

--- infra/modules/application_insights/outputs.tf
+++ infra/modules/application_insights/outputs.tf
@@ -1,3 +1,3 @@
-output "app_insights_instrumentation_key" {
-  value = azurerm_application_insights.appinsights.instrumentation_key
+output "app_insights_connection_string" {
+  value = azurerm_application_insights.appinsights.connection_string
 }
