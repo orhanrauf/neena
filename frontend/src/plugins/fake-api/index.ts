8677318c80005799dff42b344f15930c055945df
Rauf <rauf@neena.io>
2024-02-11 13:44:52 +0100

    Disable mockServiceWorker

--- frontend/src/plugins/fake-api/index.ts
+++ frontend/src/plugins/fake-api/index.ts
@@ -40,10 +40,10 @@ const worker = setupWorker(
 export default function () {
   const workerUrl = `${import.meta.env.BASE_URL ?? '/'}mockServiceWorker.js`
 
-  worker.start({
-    serviceWorker: {
-      url: workerUrl,
-    },
-    onUnhandledRequest: 'bypass',
-  })
+  // worker.start({
+  //   serviceWorker: {
+  //     url: workerUrl,
+  //   },
+  //   onUnhandledRequest: 'bypass',
+  // })
 }
