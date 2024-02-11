0566d0f3a7d7bbed3533059620514b8b0d1f14b9
Rauf <rauf@neena.io>
2024-02-11 13:57:37 +0100

    Add console prints to see HTTP protocol being used

--- frontend/src/composables/http.ts
+++ frontend/src/composables/http.ts
@@ -6,11 +6,16 @@ const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1/',
 });
 
+console.log('Base URL:', import.meta.env.VITE_API_BASE_URL + '/api/v1/');
+
+
 http.interceptors.request.use(config => {
   const token = store.state.auth.token;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+  console.log('Base URL:', import.meta.env.VITE_API_BASE_URL + '/api/v1/');
+
   return config;
 });
 
