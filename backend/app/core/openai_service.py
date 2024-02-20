36a13ba8c402225e9e4795753ef4531cfbd4c65e
Lennert Jansen <lennertjansen95@gmail.com>
2024-02-20 09:37:25 +0100

    feat: tested first toy integration with OpenAI API

--- backend/app/core/openai_service.py
+++ backend/app/core/openai_service.py
@@ -1,6 +1,9 @@
 import openai
 
+from openai import OpenAI
 from app.core.config import settings
+
+client = OpenAI()
 
 class OpenAIService:
     
@@ -11,11 +14,17 @@ class OpenAIService:
     def __init__(self):
         self.api_key = settings.OPENAI_API_KEY
         openai.api_key = self.api_key
-        # adjust however necessary
+        self.model = "gpt-3.5-turbo"
+        self.messages = [
+            {"role": "system", "content" : "You are an AI assistant, skilled in explaining solutions in simple effective language."},
+            {"role": "user", "content" : "How can I set up a new Trello project based on a project roadmap written in Notion?"}
+        ]
         
-    def method_that_allows_for_talking_to_openai():
+    def process_chat_completion(self):
+        return client.chat.completions.create(model=self.model, messages=self.messages)
+    
+    def extract_answer_from_response(self):
         pass
     
-    pass
-
 open_ai_service = OpenAIService()
+# print(open_ai_service.process_chat_completion())
