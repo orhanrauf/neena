18fe0b4b097f0201b832c2c1ccc4076b09e03672
Rauf <rauf@neena.io>
2024-03-06 17:38:12 +0000

    fix: adjust startup.sh explicate bin/bash and install gunicorn

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
+
 python -m venv venv
 
 source venv/bin/activate
 
 pip install poetry 
 pip install uvicorn 
-pip install gunicorn
 
 
 uvicorn --version
@@ -15,4 +16,4 @@ echo "========================================"
 echo "Running the application"
 
 
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4 --reload
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
