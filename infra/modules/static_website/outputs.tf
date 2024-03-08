0526c0f368bfb3721e6cf0c30e89f9757ef5227e
Rauf <rauf@neena.io>
2024-03-08 16:31:15 +0000

    Adjust terraform files to fix static web deployment

--- infra/modules/static_website/outputs.tf
+++ infra/modules/static_website/outputs.tf
@@ -3,11 +3,6 @@ output "static_site_default_hostname" {
   description = "The default hostname of the static site."
 }
 
-output "static_site_custom_domain_verification_id" {
-  value = azurerm_static_site.neena_core_static_app.custom_domain_verification_id
-  description = "The ID required to verify custom domains for the static site."
-}
-
 output "static_site_id" {
   value = azurerm_static_site.neena_core_static_app.id
   description = "The ID of the static site."
