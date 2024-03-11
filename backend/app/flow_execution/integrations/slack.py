15ea1c0d2b16726896ac4d614d36618cf5bc63f4
Lennert Jansen <lennertjansen95@gmail.com>
2024-03-11 15:57:27 +0100

    feat: first commit on Slack API integration feature branch.

---
+++ backend/app/flow_execution/integrations/slack.py
@@ -0,0 +1,1 @@
+from app.flow_execution.models.slack import SlackBase, SlackBaseUpdate
---
+++ backend/app/flow_execution/models/slack.py
@@ -0,0 +1,9 @@
+from app.flow_execution.models.base import BaseAPIModel, BaseIntegrationActionModel
+
+
+class SlackBase(BaseAPIModel):
+    pass
+
+
+class SlackBaseUpdate(BaseIntegrationActionModel):
+    pass
